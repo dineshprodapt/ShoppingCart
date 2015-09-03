d2c180f91b4a1abd6e33be051521f9936714a65c
dineshprodapt <dineshmsp@outlook.com>
2015-09-03 18:20:24 +0530

    commit2

--- www/js/shoppingCart.js
+++ www/js/shoppingCart.js
@@ -1,16 +1,10 @@
-﻿//----------------------------------------------------------------
-// shopping cart
-//
+﻿
 function shoppingCart(cartName) {
     this.cartName = cartName;
     this.clearCart = false;
     this.checkoutParameters = {};
     this.items = [];
-
-    // load items from local storage when initializing
     this.loadItems();
-
-    // save items to local storage when unloading
     var self = this;
     $(window).unload(function () {
         if (self.clearCart) {
@@ -21,7 +15,7 @@ function shoppingCart(cartName) {
     });
 }
 
-// load items from local storage
+
 shoppingCart.prototype.loadItems = function () {
     var items = localStorage != null ? localStorage[this.cartName + "_items"] : null;
     if (items != null && JSON != null) {
@@ -36,12 +30,12 @@ shoppingCart.prototype.loadItems = function () {
             }
         }
         catch (err) {
-            // ignore errors while loading...
+            
         }
     }
 }
 
-// save items to local storage
+
 shoppingCart.prototype.saveItems = function () {
     if (localStorage != null && JSON != null) {
         localStorage[this.cartName + "_items"] = JSON.stringify(this.items);
@@ -122,120 +116,7 @@ shoppingCart.prototype.addCheckoutParameters = function (serviceName, merchantID
     this.checkoutParameters[serviceName] = new checkoutParameters(serviceName, merchantID, options);
 }
 
-// check out
-shoppingCart.prototype.checkout = function (serviceName, clearCart) {
 
-    // select serviceName if we have to
-    if (serviceName == null) {
-        var p = this.checkoutParameters[Object.keys(this.checkoutParameters)[0]];
-        serviceName = p.serviceName;
-    }
-
-    // sanity
-    if (serviceName == null) {
-        throw "Use the 'addCheckoutParameters' method to define at least one checkout service.";
-    }
-
-    // go to work
-    var parms = this.checkoutParameters[serviceName];
-    if (parms == null) {
-        throw "Cannot get checkout parameters for '" + serviceName + "'.";
-    }
-    switch (parms.serviceName) {
-        case "PayPal":
-            this.checkoutPayPal(parms, clearCart);
-            break;
-        case "Google":
-            this.checkoutGoogle(parms, clearCart);
-            break;
-        case "Stripe":
-            this.checkoutStripe(parms, clearCart);
-            break;
-        default:
-            throw "Unknown checkout service: " + parms.serviceName;
-    }
-}
-
-// check out using PayPal
-// for details see:
-// www.paypal.com/cgi-bin/webscr?cmd=p/pdn/howto_checkout-outside
-shoppingCart.prototype.checkoutPayPal = function (parms, clearCart) {
-
-    // global data
-    var data = {
-        cmd: "_cart",
-        business: parms.merchantID,
-        upload: "1",
-        rm: "2",
-        charset: "utf-8"
-    };
-
-    // item data
-    for (var i = 0; i < this.items.length; i++) {
-        var item = this.items[i];
-        var ctr = i + 1;
-        data["item_number_" + ctr] = item.sku;
-        data["item_name_" + ctr] = item.name;
-        data["quantity_" + ctr] = item.quantity;
-        data["amount_" + ctr] = item.price.toFixed(2);
-    }
-
-    // build form
-    var form = $('<form/></form>');
-    form.attr("action", "https://www.paypal.com/cgi-bin/webscr");
-    form.attr("method", "POST");
-    form.attr("style", "display:none;");
-    this.addFormFields(form, data);
-    this.addFormFields(form, parms.options);
-    $("body").append(form);
-
-    // submit form
-    this.clearCart = clearCart == null || clearCart;
-    form.submit();
-    form.remove();
-}
-
-// check out using Google Wallet
-// for details see:
-// developers.google.com/checkout/developer/Google_Checkout_Custom_Cart_How_To_HTML
-// developers.google.com/checkout/developer/interactive_demo
-shoppingCart.prototype.checkoutGoogle = function (parms, clearCart) {
-
-    // global data
-    var data = {};
-
-    // item data
-    for (var i = 0; i < this.items.length; i++) {
-        var item = this.items[i];
-        var ctr = i + 1;
-        data["item_name_" + ctr] = item.sku;
-        data["item_description_" + ctr] = item.name;
-        data["item_price_" + ctr] = item.price.toFixed(2);
-        data["item_quantity_" + ctr] = item.quantity;
-        data["item_merchant_id_" + ctr] = parms.merchantID;
-    }
-
-    // build form
-    var form = $('<form/></form>');
-    // NOTE: in production projects, use the checkout.google url below;
-    // for debugging/testing, use the sandbox.google url instead.
-    //form.attr("action", "https://checkout.google.com/api/checkout/v2/merchantCheckoutForm/Merchant/" + parms.merchantID);
-    form.attr("action", "https://sandbox.google.com/checkout/api/checkout/v2/checkoutForm/Merchant/" + parms.merchantID);
-    form.attr("method", "POST");
-    form.attr("style", "display:none;");
-    this.addFormFields(form, data);
-    this.addFormFields(form, parms.options);
-    $("body").append(form);
-
-    // submit form
-    this.clearCart = clearCart == null || clearCart;
-    form.submit();
-    form.remove();
-}
-
-// check out using Stripe
-// for details see:
-// https://stripe.com/docs/checkout
 shoppingCart.prototype.checkoutStripe = function (parms, clearCart) {
 
     // global data
@@ -254,12 +135,6 @@ shoppingCart.prototype.checkoutStripe = function (parms, clearCart) {
     // build form
     var form = $('.form-stripe');
     form.empty();
-    // NOTE: in production projects, you have to handle the post with a few simple calls to the Stripe API.
-    // See https://stripe.com/docs/checkout
-    // You'll get a POST to the address below w/ a stripeToken.
-    // First, you have to initialize the Stripe API w/ your public/private keys.
-    // You then call Customer.create() w/ the stripeToken and your email address.
-    // Then you call Charge.create() w/ the customer ID from the previous call and your charge amount.
     form.attr("action", parms.options['chargeurl']);
     form.attr("method", "POST");
     form.attr("style", "display:none;");
